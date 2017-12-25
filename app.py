211d841c1c1cdb7b1ee9862f2a5ca21aed4674d2
srafi1 <shakilrafi0@gmail.com>
2017-12-25 17:53:19 -0500

    base page

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def root():
-    return render_template("mapTest.html")
+    return render_template('index.html')
 
-if (__name__ == '__main__'):
+if __name__ == '__main__':
     app.debug = True
     app.run()
 
