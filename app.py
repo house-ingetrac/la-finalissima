3814cf4a34057e2456ccab0b2421250af3f5add8
kwang7 <kwang7@stuy.edu>
2018-01-08 12:10:10 -0500

    :boom: :poop: :boom:

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+import urllib2, json
+
 
 app = Flask(__name__)
+app.secret_key = os.urandom(32)
 
 @app.route('/')
 def root():
-    return render_template('index.html')
+    #if 'user' in session:
+    return render_template('map.html', title = "Map")
+    #else:
+     #   return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/map')
+def map():
+    return render_template('map.html', title = "Map")
 
 if __name__ == '__main__':
     app.debug = True
