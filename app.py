29033081d69683d70b3065e558836ebb1e1acbfa
Kelly Wang <kwang7@stuy.edu>
2018-01-23 23:12:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app.secret_key = os.urandom(32)
 def logged_in():
     return 'user' in session
 
+#root route
 @app.route('/')
 def root():
     if 'user' in session:
@@ -36,22 +37,22 @@ def logout():
         session.pop('user')
     return redirect('/')
 
+#create an account
 @app.route('/create',methods=['GET', 'POST'])
 def create():
     if 'user' in session:
         return redirect('/')
     else:
         users = db.getUsers()
-        # print users
-        # print request.form.get('user')
-        if request.form.get('user') in users:
+        if request.form.get('user') in users: #username already exists
             flash("u can't pick that name")
             return redirect(url_for('login'))
-        if request.form.get('password') != request.form.get('confpass'):
+        if request.form.get('password') != request.form.get('confpass'): #passwords dont match
             # print "password"
             flash("Hey ur passwords are wrong")
             return redirect(url_for('login'))
         else:
+            #success! log in and view the map
             db.addUser(request.form.get('user'), request.form.get('password'))
             session['user'] = request.form.get('user')
             # print "Made account for %s" % request.form.get('user')
@@ -63,14 +64,12 @@ def auth():
         return redirect('/')
     else:
         users = db.getUsers()
-        # print users
-        # print request.form.get('user')
         if request.form.get('user') in users:
-            # print 'here:'
-            # print users[request.form.get('user')]
+            #success! log in and view the map
             if request.form.get('password') == users[request.form.get('user')]:
                 session['user'] = request.form.get('user')
                 return redirect(url_for('map'))
+            #can not log in :(
             else:
                 flash ('thats not the right password')
                 return redirect(url_for('login'))
@@ -78,6 +77,7 @@ def auth():
             flash ('that person doesnt exist')
             return redirect(url_for('login'))
 
+#view the map
 @app.route('/map')
 def map():
     if 'user' in session:
@@ -90,6 +90,7 @@ def profile():
     if 'user' in session:
         raw_pokemon = db.getPokemon(session['user'])
         pokemon = []
+        #list of all the pokemon you've encountered and info about them
         for key in raw_pokemon:
             pokedata = pokebase.pokemon( int(key)+1)
             this_pokemon = {}
@@ -98,6 +99,8 @@ def profile():
             this_pokemon['name'] = pokedata.name.encode('ascii', 'ignore').title()
             this_pokemon['type1'] = pokedata.types[0].type.name.title()
             if len(pokedata.types) > 1:
+                #type1: this pokemon has been caught!
+                #type2: encountered but not caught
                 this_pokemon['type2'] = pokedata.types[1].type.name.title()
             else:
                 this_pokemon['type2'] = ''
@@ -115,6 +118,7 @@ def profile():
     
 @app.route('/load_encounter')
 def load_encounter():
+    #spawn pokemon based on rarity
     rarity_list = [1]*10 + [2]*9 + [3]*8 + [4]*7 + [5]*6 + [6]*5 + [7]*4 + [8]*3 + [9]*2 + [10]
     rarity = random.choice(rarity_list)
     pokemon_choice = db.getPokemonWithRarity(rarity)
@@ -130,13 +134,14 @@ def set_capture(pokemon_id):
     session['encounter'] = pokemon_id
     return ''
 
+#encounter a pokemon
 @app.route('/capture')
 def capture():
     if 'user' in session:
         pokemon_id = session['encounter']
         pokemon_id = int(pokemon_id)
         pokemon = pokebase.pokemon(pokemon_id)
-        db.addPokemon(session['user'], pokemon_id - 1, False)
+        db.addPokemon(session['user'], pokemon_id - 1, False) #false- pokemon has been encountered, not caught
         return render_template('capture.html',
                 sprite = pokemon.sprites.front_default,
                 name = pokemon.name,
@@ -144,12 +149,14 @@ def capture():
                 log = True)
     return redirect('/')
 
+#update pokemon blob from users table
 @app.route('/caught')
 def caught():
     #player caught the pokemon in session['encounter']
     pokemon_id = session['encounter']
     pokemon_id = int(pokemon_id)
     db.addPokemon(session['user'], pokemon_id - 1, True)
+    #success! go back to the map
     return redirect('/map')
 
 
