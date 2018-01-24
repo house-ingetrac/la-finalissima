e77e41802a3c3e423c04dd96fae095ed6ccfa260
Kelly Wang <kwang7@stuy.edu>
2018-01-23 23:20:16 -0500

    Update poke.py

--- utils/poke.py
+++ utils/poke.py
@@ -1,8 +1,6 @@
 import urllib2, json, random, pokebase, os
 
-
-
-
+#use pokebase package to get info about this pokemon
 def findPokemon(num):
     poke = pokebase.pokemon(num) 
     info = {}
@@ -11,9 +9,9 @@ def findPokemon(num):
     info["Sprite"] = pokebase.pokemon_sprite(num).path
     return info
 
-
+'''
 info = findPokemon(206)
 for key in info:
     print key + ": "
     print info[key]
-
+'''
