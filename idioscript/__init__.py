d42666feeb6186ffb2697a8e1b0b6b87c7c9836b
srafi1 <shakilrafi0@gmail.com>
2018-05-22 17:44:00 -0400

    init actual project

--- idioscript/__init__.py
+++ idioscript/__init__.py
@@ -1,22 +1,10 @@
-from flask import Flask, url_for
-from os import path
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-DIR = path.dirname(__file__)
-
-
-#console output will appear in /var/log/apache2/error.log
-
 @app.route('/')
 def root():
-    print "=====================================\nConsole Message\n"
-    print DIR + "\n====================================="
-    body = "<h2> Deployment Test </h2>"
-    body+= "DIR: " + DIR + "<br>"
-    body+= '<img src="' + url_for('static', filename='img/sacrifice.jpg') + '" width="500"</img>'
-    body+= '<br>idioscript'
-    return body
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
